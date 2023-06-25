4407d89540b8af27ae45eb43616224cd8c80d36b
Geeks-Vegeta <shreyasmohite786@gmail.com>
2023-06-25 16:47:08 +0530

    loading and isst added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 
 
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+         document.getElementById('main-section').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+           document.getElementById('interactive');
+           document.getElementById('load').style.visibility="hidden";
+           document.getElementById('main-section').style.visibility="visible";
+        },4000);
+    }
+  }
+
 window.onload = function() {
+
+    setTimeout(()=>{
+        console.log("don")
+
+    },3000)
+
     let gslv_button=document.getElementById("rocket");
     gslv_button.addEventListener("click",function(){
         launchRocketCountDown()
