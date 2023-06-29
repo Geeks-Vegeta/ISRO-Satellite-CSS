26ed3ab923973d3cd640831ddea67ff40a0e4937
Geeks-Vegeta <shreyasmohite786@gmail.com>
2023-06-29 19:34:22 +0530

    button added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-
+function autoTyping(text,elements, delay) {
+    let inputElement=document.getElementById(elements);
+    var index = 0;
+  
+    // Function to type a single character
+    function typeCharacter() {
+      if (index < text.length) {
+        var char = text.charAt(index);
+        inputElement.innerHTML += char;
+        index++;
+        setTimeout(typeCharacter, delay);
+      }
+    }
+  
+    // Clear the input field before typing
+    inputElement.innerHTML = '';
+  
+    // Start typing
+    typeCharacter();
+  }
+  
 
 document.onreadystatechange = function () {
     var state = document.readyState
@@ -13,19 +33,46 @@ document.onreadystatechange = function () {
     }
   }
 
-window.onload = function() {
 
-    setTimeout(()=>{
-        console.log("don")
+const LaunchingRocket=()=>{
+  document.getElementById("lauch").disabled=true;
+  document.getElementById("lauch").style.display="none";
 
-    },3000)
 
-    let gslv_button=document.getElementById("rocket");
-    gslv_button.addEventListener("click",function(){
-        launchRocketCountDown()
+  launchRocketCountDown();
 
-    })
-  };
+  setTimeout(() => {
+      document.getElementById("writing-board").style.display='block';
+      let gslv=document.getElementById("gslv");
+      let img = document.createElement("img");
+      img.setAttribute("id","rocket");
+      img.setAttribute("class", "gslv");
+      img.src="../images/gslv.png";
+      gslv.appendChild(img)
+
+      autoTyping("hello shreyas","write", 100);
+
+    
+  }, 11000);
+
+
+}
+
+// window.onload = function() {
+
+
+//     let gslv_button=document.getElementById("rocket");
+//     gslv_button.addEventListener("click",function(){
+
+//         launchRocketCountDown();
+
+//         setTimeout(() => {
+//             document.getElementById("writing-board").style.display='block';
+//             autoTyping("hello", 100);
+//         }, 11000);
+
+//     })
+//   };
 
 
 const launchRocketCountDown=()=>{
